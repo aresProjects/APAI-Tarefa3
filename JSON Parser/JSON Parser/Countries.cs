fa718bd1b4a71668accf4f75bd42ae32c9c23a7b
Rui <ruipsilva2001@hotmail.com>
2020-03-27 17:07:29 +0000

    Added JsonParser (Initial Commit)

---
+++ JSON Parser/JSON Parser/Countries.cs
@@ -0,0 +1,10 @@
+﻿namespace JSON_Parser
+{
+    public class Countries
+    {
+        public string Date { get; set; }
+        public int Confirmed { get; set; }
+        public int Deaths { get; set; }
+        public int Recovered { get; set; }
+    }
+}

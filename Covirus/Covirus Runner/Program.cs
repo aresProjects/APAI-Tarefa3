463ba06d2ebc4cf5b8db59952042ce351ff6ded5
De4dSh0t <ruipsilva2001@hotmail.com>
2020-03-29 17:37:51 +0100

    Covirus v0.3 (Updated Class organization)

--- Covirus/Covirus Runner/Program.cs
+++ Covirus/Covirus Runner/Program.cs
@@ -9,10 +9,24 @@ namespace Covirus_Runner
         static void Main(string[] args)
         {
             DownloaderWebClient client = new DownloaderWebClient();
-            JsonParser jParser = new JsonParser();
+            JsonParser covid = new JsonParser();
 
             client.DownloadFile("https://pomber.github.io/covid19/timeseries.json", "covid19.json");
-            jParser.Parser("covid19.json");
+            covid.Parser("covid19.json");
+            
+            
+            foreach (KeyValuePair<string, List<Countries>> c in covid.Country) //Percorre o dicionário
+            {
+                if (c.Key == "Portugal") //Se o país (Key) for igual a "Portugal", então dou print do nome do país e mostro o nº de confirmados por data
+                {
+                    Console.WriteLine($"Country: {c.Key}");
+                
+                    foreach (Countries c1 in c.Value)
+                    {
+                        Console.WriteLine($"{c1.Date}: {c1.Confirmed}");
+                    }
+                }
+            }
         }
     }
 }
--- Covirus/Covirus/JsonParser.cs
+++ Covirus/Covirus/JsonParser.cs
@@ -7,25 +7,16 @@ namespace Covirus
 {
     public class JsonParser
     {
+        private Dictionary<string, List<Countries>> country;
+
+        public Dictionary<string, List<Countries>> Country { get => country; }
+
         public void Parser(string file)
         {
             string content = File.ReadAllText(file); //Lê o ficheiro "timeseries.json" na sua totalidade
             
             //Dicionário de Países(Key) e as respetivas mortes/infetados/recuperados (Values)
-            Dictionary<string, List<Countries>> country = JsonConvert.DeserializeObject < Dictionary<string, List<Countries>> >(content);
-            
-            foreach (KeyValuePair<string, List<Countries>> c in country) //Percorre o dicionário
-            {
-                if (c.Key == "Portugal") //Se o país (Key) for igual a "Portugal", então dou print do nome do país e mostro o nº de confirmados por data
-                {
-                    Console.WriteLine($"Country: {c.Key}");
-                
-                    foreach (Countries c1 in c.Value)
-                    {
-                        Console.WriteLine($"{c1.Date}: {c1.Confirmed}");
-                    }
-                }
-            }
+            country = JsonConvert.DeserializeObject < Dictionary<string, List<Countries>> >(content);
         }
     }
 }
